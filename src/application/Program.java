f2607b14512bc4296b1bd58ffee97b0461e480c6
Marcello Venturini <75899197+marcelloventurini@users.noreply.github.com>
2022-08-11 10:07:18 -0300

    Add files via upload

---
+++ src/application/Program.java
@@ -0,0 +1,28 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Enter the number quantity: ");
+    int n = scanner.nextInt();
+    int[] vect = new int[n];
+
+    for (int i = 0; i < vect.length; i++) {
+      System.out.print("Enter a number: ");
+      vect[i] = scanner.nextInt();
+    }
+
+    System.out.println("Negative numbers:");
+
+    for (int i = 0; i < vect.length; i++) {
+      if (vect[i] < 0) {
+        System.out.println(vect[i]);
+      }
+    }
+
+    scanner.close();
+  }
+}
