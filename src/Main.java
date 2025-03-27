7c903cb735b0447d5688f385d64c93a11e66e200
Marcello Venturini <marcellofventurini@gmail.com>
2025-03-27 15:01:26 -0300

    project completed - final commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("How many numbers will you enter? ");
+        int n = scanner.nextInt();
+        int vector[] = new int[n];
+
+        for (int i = 0; i < vector.length; i++) {
+            System.out.print("Enter a number: ");
+            vector[i] = scanner.nextInt();
+        }
+
+        System.out.println("Negative numbers:");
+
+        for (int i = 0; i < vector.length; i++) {
+            if (vector[i] < 0) {
+                System.out.println(vector[i]);
+            }
+        }
+
+        scanner.close();
     }
 }
